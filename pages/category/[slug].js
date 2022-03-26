6ec63f0ed3497f4865ec8ce881869a9acc9cb9de
Huzaifa Aftab <75427330+huzaifa95-star@users.noreply.github.com>
2022-03-26 13:55:13 +0500

    Update [slug].js

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -31,7 +31,7 @@ const CategoryPost = ({ posts }) => {
 export default CategoryPost;
 
 // Fetch data at build time
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
   const posts = await getCategoryPost(params.slug);
 
   return {
@@ -39,12 +39,4 @@ export async function getStaticProps({ params }) {
   };
 }
 
-// Specify dynamic routes to pre-render pages based on data.
-// The HTML is generated at build time and will be reused on each request.
-export async function getStaticPaths() {
-  const categories = await getCategories();
-  return {
-    paths: categories.map(({ slug }) => ({ params: { slug } })),
-    fallback: true,
-  };
-}
+
